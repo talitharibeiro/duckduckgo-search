27fe8d8a775038d8d00ede961374ea79518f6334
Talitha Ribeiro <talitha.reclaim@gmail.com>
2024-12-01 15:01:31 -0300

    feat: add post method

--- backend/src/controllers/search.controller.ts
+++ backend/src/controllers/search.controller.ts
@@ -1,17 +1,40 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common';
+import { SearchQueryDto } from 'src/dtos/search.dto';
 import { SearchService } from 'src/services/search.service';
 
 @Controller('search')
 export class SearchController {
   constructor(private readonly searchService: SearchService) {}
 
-  // Route to search on DuckDuckGo
+  /**
+   * GET /search
+   * Performs a search using the DuckDuckGo API with a query parameter `q`.
+   */
   @Get()
   async search(@Query('q') query: string) {
     return this.searchService.searchDuckDuckGo(query);
   }
 
-  // Route to get search history
+  /**
+   * POST /search
+   * Performs a search using the DuckDuckGo API with a query in the request body.
+   */
+  @Post()
+  async searchWithBody(@Body(new ValidationPipe()) body: SearchQueryDto) {
+    return this.searchService.searchDuckDuckGo(body.query);
+  }
+
+  /**
+   * GET /search/history
+   * Retrieves the search history stored in the server.
+   */
   @Get('history')
   getHistory() {
     return this.searchService.getHistory();
